0504544affe3da0d556abf42bb6abd8f3ac7da44
pavelShen <pavelShen@foxmail.com>
2016-09-01 00:06:14 +0800

    剥离route模块

--- main.js
+++ main.js
@@ -1,38 +1,12 @@
 'use strict';
 
-let koa = require('koa');
-let Router = require('koa-router');
-let render = require('koa-ejs');
 let path = require('path');
+let koa = require('koa');
+let render = require('koa-ejs');
+let myRouter = require('./route.js');
 let logger = require('./log.js');
 
 let app = koa();
-let myRouter = new Router();
-
-let restaurantData = require('./controller/restaurant.js');
-
-myRouter.get('user','/users/:id', function *(next) {
-  yield this.render('user',{
-    userInfo:{
-        id:this.params.id,
-        text:'textFont'
-    }
-  });
-});
-
-myRouter.get('detail','/detail/:id',function *(next){
-   yield this.render('detail',{
-    text:'abc'
-   });
-})
-
-myRouter.get('notFound','/404', function *(next) {
-  yield this.render('404',{
-    text:'a'
-  });
-});
-
-myRouter.get('root','/', restaurantData);
 
 render(app, {
   root: path.join(__dirname, 'view'),
---
+++ route.js
@@ -0,0 +1,30 @@
+let koa = require('koa');
+let Router = require('koa-router');
+
+let myRouter = new Router();
+let restaurantData = require('./controller/restaurant.js');
+
+myRouter.get('user','/users/:id', function *(next) {
+  yield this.render('user',{
+    userInfo:{
+        id:this.params.id,
+        text:'textFont'
+    }
+  });
+});
+
+myRouter.get('detail','/detail/:id',function *(next){
+   yield this.render('detail',{
+    text:'abc'
+   });
+})
+
+myRouter.get('notFound','/404', function *(next) {
+  yield this.render('404',{
+    text:'a'
+  });
+});
+
+myRouter.get('root','/', restaurantData);
+
+module.exports = myRouter;
